42e917df9be39292bb011dbf632482e7ffb06ceb
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-04-22 20:29:28 +0200

    Script update: R,G,B factors and LO,HI cuts
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- combine.sh
+++ combine.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 for f in $*
 do
-  LIB="jpegbw.so" F="alpha(x1, 6.28, -.1, .9)" jpegbw "$f"
+  LO=4 HI=4 R=0.25 G=0.6 B=0.15 LIB="jpegbw.so" F="alpha(x1, 6.28, -.1, .9)" jpegbw "$f"
   mv "bw_$f" "r_$f"
-  LIB="jpegbw.so" F="alpha(x1, 6.48, 0., 1.)" jpegbw "$f"
+  LO=4 HI=4 R=0.25 G=0.6 B=0.15 LIB="jpegbw.so" F="alpha(x1, 6.48, 0., 1.)" jpegbw "$f"
   mv "bw_$f" "g_$f"
-  LIB="jpegbw.so" F="alpha(x1, 6.68, .1, 1.1)" jpegbw "$f"
+  LO=4 HI=4 R=0.25 G=0.6 B=0.15 LIB="jpegbw.so" F="alpha(x1, 6.68, .1, 1.1)" jpegbw "$f"
   mv "bw_$f" "b_$f"
-  LIB="jpegbw.so" F="cbrt(vingette(1., x2, x3))" jpegbw "$f"
-  :q
+  LO=4 HI=4 R=0.25 G=0.6 B=0.15 LIB="jpegbw.so" F="cbrt(vingette(1., x2, x3))" jpegbw "$f"
   mv "bw_$f" "a_$f"
   convert "r_$f" "g_$f" "b_$f" "a_$f" -combine "out_$f"
   rm "r_$f" "g_$f" "b_$f" "a_$f"
