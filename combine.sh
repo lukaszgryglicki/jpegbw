180b12e4124b68ff9c940a0214589d9957e9256b
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-04-22 20:27:26 +0200

    support alpha channel
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- combine.sh
+++ combine.sh
@@ -7,6 +7,9 @@ do
   mv "bw_$f" "g_$f"
   LIB="jpegbw.so" F="alpha(x1, 6.68, .1, 1.1)" jpegbw "$f"
   mv "bw_$f" "b_$f"
-  convert "r_$f" "g_$f" "b_$f" -combine "out_$f"
-  rm "r_$f" "g_$f" "b_$f"
+  LIB="jpegbw.so" F="cbrt(vingette(1., x2, x3))" jpegbw "$f"
+  :q
+  mv "bw_$f" "a_$f"
+  convert "r_$f" "g_$f" "b_$f" "a_$f" -combine "out_$f"
+  rm "r_$f" "g_$f" "b_$f" "a_$f"
 done
