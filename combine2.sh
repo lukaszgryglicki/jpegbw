5df8b909733484cf1787823caebbfe8f5dfa0bba
lukaszgryglicki <lukaszgryglicki@o2.pl>
2018-04-29 14:59:59 +0200

    lib name changed - update old scripts
    Signed-off-by: lukaszgryglicki <lukaszgryglicki@o2.pl>

--- combine2.sh
+++ combine2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 for f in $*
 do
-  LO=4 HI=4 R=1 G=0 B=0 LIB="jpegbw.so" F="alpha(1.-x1, 6.28319, -0.05, 1.39)" jpegbw "$f"
+  LO=4 HI=4 R=1 G=0 B=0 LIB="libjpegbw.so" F="alpha(1.-x1, 6.28319, -0.05, 1.39)" jpegbw "$f"
   mv "bw_$f" "r_$f"
-  LO=4 HI=4 R=0 G=1 B=0 LIB="jpegbw.so" F="alpha(1.-x1, 6.28319, 0., 1.4)" jpegbw "$f"
+  LO=4 HI=4 R=0 G=1 B=0 LIB="libjpegbw.so" F="alpha(1.-x1, 6.28319, 0., 1.4)" jpegbw "$f"
   mv "bw_$f" "g_$f"
-  LO=4 HI=4 R=0 G=0 B=1 LIB="jpegbw.so" F="alpha(1.-x1, 6.28319, .05, 1.41)" jpegbw "$f"
+  LO=4 HI=4 R=0 G=0 B=1 LIB="libjpegbw.so" F="alpha(1.-x1, 6.28319, .05, 1.41)" jpegbw "$f"
   mv "bw_$f" "b_$f"
   convert "r_$f" "g_$f" "b_$f" -combine "out_$f"
   rm "r_$f" "g_$f" "b_$f" 
