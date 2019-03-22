40b72811e6cc3f983aa70b4312b60f3d4df5a306
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-22 18:54:49 +0100

    Optimize some scripts
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- alien1.sh
+++ alien1.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-LIB=./libjpegbw.so RR=2 RG=1 RB=1 GR=1 GG=2 GB=1 BR=1 BG=1 BB=2 RLO=3 RHI=3 GLO=3 GHI=3 BLO=3 BHI=3 NA=1 RF="sin(3.1416*x1)" GF="sin(3.1416*x1)" BF="sin(3.1416*x1)" jpeg $*
+LIB=./libjpegbw.so RC=1 GC=1 BC=1 RR=2 RG=1 RB=1 GR=1 GG=2 GB=1 BR=1 BG=1 BB=2 RLO=3 RHI=3 GLO=3 GHI=3 BLO=3 BHI=3 NA=1 RF="sin(3.1416*x1)" GF="sin(3.1416*x1)" BF="sin(3.1416*x1)" jpeg $*
--- alien3.sh
+++ alien3.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-LIB=./libjpegbw.so RR=1 RG=0 RB=0 GR=0 GG=1 GB=0 BR=0 BG=0 BB=1 RLO=1 RHI=1 GLO=1 GHI=1 BLO=1 BHI=1 AR=0.2125 AG=0.7154 AB=0.0721 ALO=1 AHI=1 AF="1-.4*cabs(2*x2-1_1)" RF="sin(3.1416*x1)" GF="sin(3.1416*x1)" BF="sin(3.1416*x1)" jpeg $*
+LIB=./libjpegbw.so RC=1 GC=1 BC=1 RR=1 RG=0 RB=0 GR=0 GG=1 GB=0 BR=0 BG=0 BB=1 RLO=1 RHI=1 GLO=1 GHI=1 BLO=1 BHI=1 AR=0.2125 AG=0.7154 AB=0.0721 ALO=1 AHI=1 AF="1-.4*cabs(2*x2-1_1)" RF="sin(3.1416*x1)" GF="sin(3.1416*x1)" BF="sin(3.1416*x1)" jpeg $*
