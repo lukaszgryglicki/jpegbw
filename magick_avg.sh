480a9e1b388f9755141d8e6c2c78429edcc18986
Lukasz Gryglicki <lgryglicki@cncf.io>
2026-04-14 20:54:58 +0200

    Middle ogf images avg/geom avg
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

---
+++ magick_avg.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 input1 input2 output"
+  exit 1
+fi
+
+in1="$1"
+in2="$2"
+out="$3"
+
+magick "$in1" "$in2" \
+  -evaluate-sequence Mean \
+  "$out"
